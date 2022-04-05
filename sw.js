ad444e4623977f5719e329e24ba8c4c9836933c2
Bushra Shaikh <bushra.sk@siesgst.ac.in>
2022-04-05 14:07:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -46,9 +46,11 @@
 
       // Code to handle install prompt on desktop
 
-// let deferredPrompt;
-// const addBtn = document.querySelector('.add-button');
-// addBtn.style.display = 'none';
+
+    let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
